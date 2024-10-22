44191020756f373e7695da9c87d60997ea1d5f56
PhilCand <philippe.canduro@gmail.com>
2024-10-23 00:30:29 +0200

    Prices, navigation

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+const navButtons = document.querySelectorAll(".nav-link");
 
 menuOpenButton.addEventListener("click", () => {
   document.body.classList.toggle("show-mobile-menu");
 });
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navButtons.forEach((button) => {
+  button.addEventListener("click", () =>
+    document.body.classList.remove("show-mobile-menu")
+  );
+});
